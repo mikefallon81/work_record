05da066fdbe444f774ddecec27da5a4419ba564e
mikefallon81 <mikefallon81@gmail.com>
2021-01-25 15:25:34 +0000

    initial commit

---
+++ server/src/main/java/mikefallonit/workrecord/WorkRecordApplication.java
@@ -0,0 +1,13 @@
+package mikefallonit.workrecord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkRecordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkRecordApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/mikefallonit/workrecord/WorkRecordApplicationTests.java
@@ -0,0 +1,13 @@
+package mikefallonit.workrecord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkRecordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
