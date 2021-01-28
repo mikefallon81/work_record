4366210e0c9140e2416518c3a09021b8e01b3d5c
mikefallon81 <mikefallon81@gmail.com>
2021-01-28 11:49:20 +0000

    test for adding job created and passing

--- server/src/test/java/mikefallonit/workrecord/WorkRecordApplicationTests.java
+++ server/src/test/java/mikefallonit/workrecord/WorkRecordApplicationTests.java
@@ -1,13 +1,33 @@
 package mikefallonit.workrecord;
 
+import mikefallonit.workrecord.models.Job;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.runner.RunWith;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import mikefallonit.workrecord.repositories.JobRepository;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class WorkRecordApplicationTests {
+public class WorkRecordApplicationTests {
+
+	@Autowired
+	JobRepository jobRepository;
+
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+	}
+
+	@Test
+	public void createJob(){
+		Job job1 = new Job("Joanna Fallon", 20.00, 10);
+		jobRepository.save(job1);
+		assertEquals(1, jobRepository.count());
 	}
 
 }
