ed108fc644b547c897eb4a6758a37f5b20c23a7b
mikefallon81 <mikefallon81@gmail.com>
2021-01-28 14:17:42 +0000

    Job Controller created

---
+++ server/src/main/java/mikefallonit/workrecord/controllers/JobController.java
@@ -0,0 +1,32 @@
+package mikefallonit.workrecord.controllers;
+
+
+import mikefallonit.workrecord.models.Job;
+import mikefallonit.workrecord.repositories.JobRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping(value="/jobs")
+
+public class JobController {
+    @Autowired
+    JobRepository jobRepository;
+
+    @GetMapping
+    public List<Job> getAllJobs(){
+        return jobRepository.findAll();
+    }
+
+    @GetMapping("{id")
+    public Optional<Job> getJob(@PathVariable Long id){
+        return jobRepository.findById(id);
+    }
+
+}
