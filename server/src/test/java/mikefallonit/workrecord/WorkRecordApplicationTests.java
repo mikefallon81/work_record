7e915f2eb958be7d3f4f5fd2a413e749a3e31c5c
mikefallon81 <mikefallon81@gmail.com>
2021-02-17 11:58:19 +0000

    Tests updated to reflect model changes

--- server/src/test/java/mikefallonit/workrecord/WorkRecordApplicationTests.java
+++ server/src/test/java/mikefallonit/workrecord/WorkRecordApplicationTests.java
@@ -1,6 +1,5 @@
 package mikefallonit.workrecord;
 
-
 import mikefallonit.workrecord.models.Activity;
 import mikefallonit.workrecord.models.Customer;
 import mikefallonit.workrecord.models.Expenditure;
@@ -8,17 +7,20 @@ import mikefallonit.workrecord.models.Job;
 import mikefallonit.workrecord.repositories.ActivityRepository;
 import mikefallonit.workrecord.repositories.CustomerRepository;
 import mikefallonit.workrecord.repositories.ExpenditureRepository;
+import mikefallonit.workrecord.repositories.JobRepository;
+
+import org.junit.After;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import mikefallonit.workrecord.repositories.JobRepository;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = WorkRecordApplication.class)
 
 public class WorkRecordApplicationTests {
 
@@ -34,32 +36,32 @@ public class WorkRecordApplicationTests {
 	@Autowired
 	ActivityRepository activityRepository;
 
-	@org.junit.Test
+	@Test
 	public void contextLoads() {
 	}
 
 
-	@org.junit.Test
+	@Test
 	public void createCustomer(){
-		Customer customer0 = new Customer("Stephen Loy Building Services", "stephenloy@hotmail.com", "0456987413", "Mount vernon road", "Inch", "Edinburgh", "EH16 7hr");
+		Customer customer0 = new Customer("Stephen Loy Building Services", "stephenloy@hotmail.com", "0456987413");
 		customerRepository.save(customer0);
-		assertEquals(4, customerRepository.count());
+		assertEquals(1, customerRepository.count());
 	}
 
-	@org.junit.Test
+	@Test
 	public void createJob(){
-		Customer customer1 = new Customer("Joanna Fallon", "joannafallon@hotmail.com", "07918994772", "53  Gilmour Road", "Newington", "Edinburgh", "EH16 5NU");
+		Customer customer1 = new Customer("Joanna Fallon", "joannafallon@hotmail.com", "07918994772");
 		customerRepository.save(customer1);
-		Job job1 = new Job(customer1, 8.00, 5.00);
+		Job job1 = new Job("IT", "01/05/2021", "PC broken", customer1, 50.00, "05/01/2021", "06/01/2021");
 		jobRepository.save(job1);
-		assertEquals(2, jobRepository.count());
+		assertEquals(1, jobRepository.count());
 	}
 
-	@org.junit.Test
+	@Test
 	public void createExpense(){
-		Customer customer1 = new Customer("Joanna Fallon", "joannafallon@hotmail.com", "07918994772", "53  Gilmour Road", "Newington", "Edinburgh", "EH16 5NU");
+		Customer customer1 = new Customer("Joanna Fallon", "joannafallon@hotmail.com", "07918994772");
 		customerRepository.save(customer1);
-		Job job1 = new Job(customer1, 20.00, 10);
+		Job job1 = new Job("IT", "01/05/2021", "PC broken", customer1, 50.00, "05/01/2021", "06/01/2021");
 		jobRepository.save(job1);
 		Expenditure expenditure1 = new Expenditure("29/01/2021", "Materials", job1, 20.00);
 		expenditureRepository.save(expenditure1);
@@ -69,14 +71,15 @@ public class WorkRecordApplicationTests {
 
 	}
 
-	@org.junit.Test
+	@Test
 	public void createActivity(){
-		Customer customer1 = new Customer("Joanna Fallon", "joannafallon@hotmail.com", "07918994772", "53  Gilmour Road", "Newington", "Edinburgh", "EH16 5NU");
+		Customer customer1 = new Customer("Joanna Fallon", "joannafallon@hotmail.com", "07918994772");
 		customerRepository.save(customer1);
-		Job job1 = new Job(customer1, 20.00, 10);
+		Job job1 = new Job("IT", "01/05/2021", "PC broken", customer1, 50.00, "05/01/2021", "06/01/2021");
 		jobRepository.save(job1);
 		Activity activity1 = new Activity("01/02/2021", 5.0, job1, "Rerouted gas pipes, installed gas fire");
 		activityRepository.save(activity1);
+		assertEquals(1, activityRepository.count());
 	}
 
 
